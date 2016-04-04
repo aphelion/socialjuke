be7d5b3fd2b9cb19faa9719bebe570bb4ed9a08d
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-04-04 19:19:30 +0200

    Make test result copy script more robust
    [#116843043]

--- ares/ci/test-results.sh
+++ ares/ci/test-results.sh
@@ -2,4 +2,10 @@
 
 cd "$(dirname "$0")/.."
 
-cp -r ./app/build/outputs/androidTest-results/* $CIRCLE_TEST_REPORTS
+if [ ! -z ${CIRCLE_TEST_REPORTS+x} ] && [ -d $CIRCLE_TEST_REPORTS ]
+then
+    echo Copying test results to \"$CIRCLE_TEST_REPORTS\".
+    cp -r ./app/build/outputs/androidTest-results/* "$CIRCLE_TEST_REPORTS"
+else
+    echo CIRCLE_TEST_REPORTS not to a directory that exists: \"$CIRCLE_TEST_REPORTS\". Not copying test results.
+fi
