d33dbea3a1b84157c11e35e9561f6aa3c33350a9
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-04-04 21:48:02 +0200

    Make the secrets directory during deployment if it doesn't exist
    [#116843043]

--- ares/ci/decode-secrets.sh
+++ ares/ci/decode-secrets.sh
@@ -2,5 +2,6 @@
 
 cd "$(dirname "$0")/.."
 
+mkdir -p ./secrets
 echo $ARES_KEYSTORE_JKS_B64          | base64 --decode > ./secrets/ares-keystore.jks
 echo $ARES_PLAY_CREDENTIALS_JSON_B64 | base64 --decode > ./secrets/ares-play-credentials.json
