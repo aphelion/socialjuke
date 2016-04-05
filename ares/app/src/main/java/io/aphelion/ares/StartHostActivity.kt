34866d1b9dedfcceb38ffccfe14130081dbaf69d
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-04-05 22:48:16 +0200

    Allow user to input a Juke name
    [#99379236]

---
+++ ares/app/src/main/java/io/aphelion/ares/StartHostActivity.kt
@@ -0,0 +1,12 @@
+package io.aphelion.ares
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartHostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_host)
+    }
+}
