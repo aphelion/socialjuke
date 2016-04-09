7cc6161bcbf975f18990396a4a0e909003db00e4
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-04-09 09:47:22 +0200

    Copy test results on CI
    [#99379236]

--- ares/ci/test-results.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-if [ ! -z ${CIRCLE_TEST_REPORTS+x} ] && [ -d $CIRCLE_TEST_REPORTS ]
-then
-    echo Copying test results to \"$CIRCLE_TEST_REPORTS\".
-    cp -r ./ares/app/build/outputs/androidTest-results/* "$CIRCLE_TEST_REPORTS"
-else
-    echo CIRCLE_TEST_REPORTS not to a directory that exists: \"$CIRCLE_TEST_REPORTS\". Not copying test results.
-fi
