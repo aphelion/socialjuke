355917f99803e90bec3d4e5d71d9fc6d146193ab
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-06-21 20:51:07 -0500

    Remove native Android project
    [#117277187]

--- ares/ci/decode-secrets.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p ./ares/secrets
-echo $ARES_KEYSTORE_JKS_B64          | python -m base64 -d > ./ares/secrets/ares-keystore.jks
-echo $ARES_PLAY_CREDENTIALS_JSON_B64 | python -m base64 -d > ./ares/secrets/ares-play-credentials.json
