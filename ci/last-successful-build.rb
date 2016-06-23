1ce2592dae32e53d00e39c0199aa6371b64aaa91
Leonhardt Koepsell <leonhardt@aphelion.io>
2016-06-23 04:28:27 -0500

    Add error handling to script
    [#117277187]

--- ci/last-successful-build.rb
+++ ci/last-successful-build.rb
@@ -7,10 +7,17 @@ TOKEN = ENV['CIRCLE_API_TOKEN']
 BRANCH = ENV['CIRCLE_BRANCH']
 REPONAME = ENV['CIRCLE_PROJECT_REPONAME']
 
+abort('Unable to read CIRCLE_API_TOKEN') unless TOKEN
+
 projects_uri = URI("https://circleci.com/api/v1/projects?circle-token=#{TOKEN}")
 response = Net::HTTP.get(projects_uri)
 projects = JSON.parse(response)
 
+abort('Unable to parse JSON from API') unless projects
+
 project = projects.find { |project| project['reponame'] == REPONAME }
 vcs_revision = project['branches'][BRANCH]['last_success']['vcs_revision']
+
+abort('Unable to parse latest successful build') unless vcs_revision
+
 puts vcs_revision
